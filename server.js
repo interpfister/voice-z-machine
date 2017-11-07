26edba4c08f980a1014a182cc86d5d5ab1b7fd8e
Ryan Pfister <interpfister@gmail.com>
2017-11-07 09:14:36 -0500

    Dynamic port

--- server.js
+++ server.js
@@ -13,4 +13,5 @@ app.post('/', (req, res) => {
   res.send(handler(event));
 });
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
