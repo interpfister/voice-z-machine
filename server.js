59554b5b2397d412f774adb7bf53ae9558cc101c
Ryan Pfister <interpfister@gmail.com>
2017-11-07 09:18:47 -0500

    add body parser

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
 const handler = require('./index').handler;
+const bodyParser = require('body-parser');
+app.use(bodyParser);
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.post('/', (req, res) => {
+  console.log(req
   const body = req.body;
   const event = {
     httpMethod: 'POST',
